20db453c0d12a8817100fcbb7ded9f8531cbe3bb
BAEKJungHo <designjava@naver.com>
2021-10-31 18:57:46 +0900

    insert images

--- src/main/java/hello/servelt/demo/basic/HelloServlet.java
+++ src/main/java/hello/servelt/demo/basic/HelloServlet.java
@@ -1,2 +1,30 @@
-package hello.servelt.demo.basic;public class HelloServlet {
+package hello.servelt.demo.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 서블릿을 만들기 위해서 HttpServlet 을 상속 받고, @WebServlet 어노테이션을 사용해야 한다.
+ */
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
+    }
+
 }
